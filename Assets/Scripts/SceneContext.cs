ab69ef65f1794ca5abb46e256a80368697844086
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-25 13:36:08 +0400

    Added SceneContext

---
+++ Assets/Scripts/SceneContext.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SceneContext : MonoBehaviour
+{
+    public SpawnPointComponent playerSpawnPoint;
+
+    public SpawnPointComponent[] enemySpawnPoints;
+
+    public WayPointComponent[] wayPoints;
+
+    public CameraRigComponent cameraRig;
+
+    public static SceneContext instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
