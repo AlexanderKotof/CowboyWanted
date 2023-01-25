bc86b778f74b3751ada5cad5d3346d74e1851758
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-25 12:47:31 +0400

    Created StartGameScreen

---
+++ Assets/Screens/StartGameScreen.cs
@@ -0,0 +1,19 @@
+﻿using ScreenSystem.Screens;
+using System;
+using UnityEngine.UI;
+
+public class StartGameScreen : BaseScreen
+{
+    public Button startButton;
+
+    public void SetStartCallback(Action callback)
+    {
+        startButton.onClick.AddListener(callback.Invoke);
+    }
+
+    protected override void OnHide()
+    {
+        startButton.onClick.RemoveAllListeners();
+        base.OnHide();
+    }
+}
