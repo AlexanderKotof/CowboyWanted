bd2aab0091f213761b8686e8e7340f213f0bb750
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-25 12:48:12 +0400

    Created GameOverScreen

---
+++ Assets/Screens/GameOverScreen.cs
@@ -0,0 +1,19 @@
+using ScreenSystem.Screens;
+using System;
+using UnityEngine.UI;
+
+public class GameOverScreen : BaseScreen
+{
+    public Button restartButton;
+
+    public void SetRestartCallback(Action callback)
+    {
+        restartButton.onClick.AddListener(callback.Invoke);
+    }
+
+    protected override void OnHide()
+    {
+        restartButton.onClick.RemoveAllListeners();
+        base.OnHide();
+    }
+}
