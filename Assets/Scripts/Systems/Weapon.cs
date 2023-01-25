b05c46ba74a0171a81c68f74ea61c65034c1a323
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-25 13:34:35 +0400

    Implemented WeaponSystem

---
+++ Assets/Scripts/Systems/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+[Serializable]
+public class Weapon
+{
+    public float damage = 5;
+
+    public int level { get; private set; }
+
+    public Weapon(Weapon weapon)
+    {
+        damage = weapon.damage;
+        level = weapon.level;
+    }
+
+    public void Upgrade()
+    {
+        damage *= 2;
+        level++;
+    }
+}
---
+++ Assets/Scripts/Systems/WeaponSystem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class WeaponSystem
+{
+    [Serializable]
+    public class Settings
+    {
+        public Weapon weapon;
+        public int[] upgradeCosts;
+    }
+    public Weapon Weapon { get; private set; }
+
+    private Settings _settings;
+    private CoinsSystem _coinsSystem;
+
+    public event Action WeaponUpgraded;
+
+    public WeaponSystem(Settings settings, CoinsSystem coinsSystem)
+    {
+        _settings = settings;
+        _coinsSystem = coinsSystem;
+
+        Weapon = new Weapon(_settings.weapon);
+    }
+
+    public bool CanUpgrade()
+    {
+        if (_settings.upgradeCosts.Length <= Weapon.level)
+            return false;
+
+        return _coinsSystem.HasCoins(GetUpgradeCost());
+    }
+
+    public int GetUpgradeCost()
+    {
+        return _settings.upgradeCosts.Length > Weapon.level ? _settings.upgradeCosts[Weapon.level] : 0;
+    }
+
+    public int GetWeaponLevel()
+    {
+        return Weapon.level;
+    }
+
+    public void UpgradeWeapon()
+    {
+        if (!CanUpgrade())
+            return;
+
+        if (!_coinsSystem.SpendCoins(GetUpgradeCost()))
+            return;
+
+        Weapon.Upgrade();
+
+        WeaponUpgraded?.Invoke();
+    }
+}
