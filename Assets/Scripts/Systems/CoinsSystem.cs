618aad498a6a20d08f24e792d7ad74c11a0785d3
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-25 13:34:53 +0400

    Implemented CoinsSystem

---
+++ Assets/Scripts/Systems/CoinsSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class CoinsSystem : IDisposable
+{
+    public int Coins => _coins;
+    private int _coins;
+
+    public event Action<int> CoinsUpdated;
+
+    public bool HasCoins(int value)
+    {
+        return _coins >= value;
+    }
+
+    public bool SpendCoins(int count)
+    {
+        if (_coins < count)
+            return false;
+
+        _coins -= count;
+
+        CoinsUpdated?.Invoke(_coins);
+
+        return true;
+    }
+
+    public void AddCoins(int value)
+    {
+        _coins += value;
+        CoinsUpdated?.Invoke(_coins);
+    }
+
+    public void Dispose()
+    {
+        CoinsUpdated = null;
+    }
+}
