e10af2f07a4b676e291b724acd30825508c9a3cb
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-25 13:35:58 +0400

    Implemented View with simple ReactiveProperty

---
+++ Assets/Scripts/View/ReactiveProperty.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class ReactiveProperty<T> : IDisposable where T : struct
+{
+    public T Value { 
+        get 
+        { 
+            return _value; 
+        }
+        set
+        {
+            SetValue(value);
+        }
+    }
+    private T _value;
+
+    public event Action<T> OnChanged;
+
+    public void SetValue(T val)
+    {
+        _value = val;
+        OnChanged?.Invoke(_value);
+    }
+
+    public void Subscribe(Action<T> onChanged)
+    {
+        OnChanged += onChanged;
+    }
+
+    public void Unsubscribe(Action<T> onChanged)
+    {
+        OnChanged -= onChanged;
+    }
+
+    public void RemoveAllSubscribers()
+    {
+        OnChanged = null;
+    }
+
+    public void Dispose()
+    {
+        RemoveAllSubscribers();
+        _value = default;
+    }
+}
---
+++ Assets/Scripts/View/View.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class View : IDisposable
+{
+    public ReactiveProperty<int> coins = new ReactiveProperty<int>();
+
+    public ReactiveProperty<int> upgradePrice = new ReactiveProperty<int>();
+
+    public ReactiveProperty<int> pistolLevel = new ReactiveProperty<int>();
+
+    public ReactiveProperty<float> bombCooldown = new ReactiveProperty<float>();
+
+    public void Dispose()
+    {
+        coins.Dispose();
+        upgradePrice.Dispose();
+        pistolLevel.Dispose();
+        bombCooldown.Dispose();
+    }
+}
