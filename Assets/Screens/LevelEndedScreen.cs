9d8c5164e7ec7d06905e85de1f8180cdd6f41845
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-25 12:47:57 +0400

    Created LevelEndScreen

---
+++ Assets/Screens/LevelEndedScreen.cs
@@ -0,0 +1,19 @@
+﻿using ScreenSystem.Screens;
+using System;
+using UnityEngine.UI;
+
+public class LevelEndedScreen : BaseScreen
+{
+    public Button restartButton;
+
+    public void SetRestartCallback(Action callback)
+    {
+        restartButton.onClick.AddListener(callback.Invoke);
+    }
+
+    protected override void OnHide()
+    {
+        restartButton.onClick.RemoveAllListeners();
+        base.OnHide();
+    }
+}
