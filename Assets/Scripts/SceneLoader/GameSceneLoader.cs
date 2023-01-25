284f898d48022dbe55856cab0cbbbdf97d4c9f12
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-25 13:35:19 +0400

    Implemented GameSceneLoader

---
+++ Assets/Scripts/SceneLoader/GameSceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UI.Screens;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneLoader
+{
+    private const string _gameSceneName = "Game";
+
+    private static Action _callback;
+
+    public static void LoadGameScene(Action callback)
+    {
+        _callback = callback;
+        ScreenSystem.ScreensManager.ShowScreen<LoadingScreen>();
+
+        var operation = SceneManager.LoadSceneAsync(_gameSceneName);
+        operation.completed += OnLoaded;
+    }
+
+    private static void OnLoaded(AsyncOperation operation)
+    {
+        operation.completed -= OnLoaded;
+
+        ScreenSystem.ScreensManager.HideScreen<LoadingScreen>();
+
+        _callback?.Invoke();
+    }
+}
