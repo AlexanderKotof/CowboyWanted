d62d479252e4240f3d8456b68e6ab0651f5d4145
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-25 13:38:08 +0400

    Implemented GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class GameController : IDisposable
+{
+    public Action shoot;
+    public Action upgradeWeapon;
+    public Action throwBomb;
+
+    public void Dispose()
+    {
+        shoot = null;
+        upgradeWeapon = null;
+        throwBomb = null;
+    }
+}
